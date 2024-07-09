b436c16b5c664b799b4c1c98486cdf1ab89bc59e
Ahmed Farah <ahmedkadar.farah@gmail.com>
2024-07-08 20:50:39 -0500

    end of section 2 template driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,8 @@ import {CreateCourseStep1Component} from './create-course/create-course-step-1/c
 import {FileUploadComponent} from './file-upload/file-upload.component';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {LoginReactiveComponent} from './login-reactive/login-reactive.component';
+import { PasswordStrengthDirective } from './directives/password-strength.directive';
+import { OnlyOneErrorPipe } from './pipes/only-one-error.pipe';
 
 @NgModule({ declarations: [
         AppComponent,
@@ -56,7 +58,9 @@ import {LoginReactiveComponent} from './login-reactive/login-reactive.component'
         CreateCourseStep3Component,
         AddressFormComponent,
         FileUploadComponent,
-        LoginReactiveComponent
+        LoginReactiveComponent,
+        PasswordStrengthDirective,
+        OnlyOneErrorPipe
     ],
     bootstrap: [AppComponent], imports: [BrowserModule,
         BrowserAnimationsModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -9,6 +10,11 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
 
+  val = {
+    email: "hello@gmail.com",
+    password: "123456"
+  }
+
   constructor() {
 
 
@@ -16,6 +22,12 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
 
+  }
+
+  onLogin(loginForm: NgForm, submit) {
+    console.log(loginForm.value, loginForm.valid, submit);
+
+    console.log("val:", this.val)
   }
 
 }
