26e3d6c37b0db680212dd8cfec7a566b1932a612
Ahmed Farah <ahmedkadar.farah@gmail.com>
2024-07-15 09:46:30 -0500

    end of section 3 intro into reactive forms

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { createaPasswordStrengthValidator } from '../validators/password-strength.validator';
 
 
 @Component({
@@ -9,13 +11,63 @@ import { Component, OnInit } from '@angular/core';
 export class LoginReactiveComponent implements OnInit {
 
 
-  constructor() {
+
+
+  // email =  new FormControl("", {
+  //   validators: [Validators.required, Validators.email],
+  //   updateOn: "blur"
+  // });
+  // password = new FormControl("", {
+  //   validators: [
+  //     Validators.required, 
+  //     Validators.minLength(8),
+  //     createaPasswordStrengthValidator()],
+  //   // updateOn: "blur"
+  // });
+
+  // form = new FormGroup({
+  //   email:this.email,
+  //   password: this.password
+  // });
+
+  form = this.fb.group({
+    email: ["", {
+      validators: [Validators.required, Validators.email],
+      updateOn: "blur"
+    }],
+
+    password: ["", [
+      Validators.required,
+      Validators.minLength(8),
+      createaPasswordStrengthValidator()
+    ]]
+  });
+
+  constructor(private fb: NonNullableFormBuilder) {
 
 
   }
 
   ngOnInit() {
 
+  }
+
+  onLogin() {
+    const value = this.form.value;
+    console.log(value)
+  }
+
+  onReset() {
+    this.form.reset();
+    console.log(this.form.value)
+  }
+
+  get email() {
+    return this.form.controls["email"];
+  }
+
+  get password() {
+    return this.form.controls["password"];
   }
 
 }
